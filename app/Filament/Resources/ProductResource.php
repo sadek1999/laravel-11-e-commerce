9fc6993ad64a2aadbd48feadf83b3f37be79cd60
sadek1999 <sadekali1530@gmail.com>
2024-12-30 22:27:26 +0600

    set products Resouces

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -2,16 +2,24 @@
 
 namespace App\Filament\Resources;
 
+use App\Enum\ProductStatusEnum;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
+use Doctrine\DBAL\Query\From;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Livewire\Features\SupportConsoleCommands\Commands\FormCommand;
+use function Laravel\Prompts\select;
 
 class ProductResource extends Resource
 {
@@ -22,8 +30,70 @@ class ProductResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
+
             ->schema([
-                //
+                Forms\Components\Grid::make()
+                ->schema([
+                    TextInput::make('title')
+                    ->live(onBlur: true)->required()
+                    ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                        $set('slug', str::slug($state));
+                    }),
+                TextInput::make('slug')->required(),
+                Select::make('department_id')
+                    ->relationship('department', 'name')
+                    ->label(__('Department'))
+                    ->searchable()
+                    ->reactive()
+                    ->preload()
+                    ->afterStateUpdated(function(callable $set){
+                        $set('category_id',null);
+                    }),
+                Select::make('category_id')
+                ->relationship(
+                   'category',
+                   'name',
+                   function(Builder $query ,callable $get){
+                    $departmentId=$get('department_id');
+                    if($departmentId){
+                        $query->where('department_id',$departmentId);
+                    }
+                   }
+                )
+                ->label(__('Category'))
+                ->preload()
+                ->searchable()
+                ->required()
+                ,
+
+                ]),
+                Forms\Components\RichEditor::make('description')
+                ->required()
+                ->toolbarButtons([
+                    'blockquote',   // Blockquote button
+                    'bold',         // Bold text button
+                    'bulletList',   // Unordered (bullet) list button
+                    'h2',           // Heading level 2 button
+                    'h3',           // Heading level 3 button
+                    'italic',       // Italic text button
+                    'link',         // Hyperlink button
+                    'orderedList',  // Ordered (numbered) list button
+                    'redo',         // Redo button
+                    'strike',       // Strikethrough text button
+                    'underline',    // Underline text button
+                    'undo',         // Undo button
+                    'table',        // Insert table button
+                ])
+                ->columnSpan(2),
+                TextInput::make('price')->required()->nullable(),
+                TextInput::make('quantity')->integer(),
+                Select::make('status')
+                ->options(ProductStatusEnum::labels())
+                ->default(ProductStatusEnum::Draft->value)
+                ->required(),
+
+
+
             ]);
     }
 
@@ -38,6 +108,7 @@ class ProductResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
