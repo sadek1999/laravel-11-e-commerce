c7540eb46a0a6cbfd4855965b1f0bb758ca0be57
sadek1999 <sadekali1530@gmail.com>
2025-01-02 22:51:30 +0600

    complate image uploade

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources;
 use App\Enum\ProductStatusEnum;
 use App\Enum\RolesEnum;
 use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\Pages\EditProduct;
+use App\Filament\Resources\ProductResource\Pages\ProductsImages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Doctrine\DBAL\Query\From;
@@ -13,8 +15,12 @@ use Filament\Forms;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Pages\Page;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
@@ -29,7 +35,8 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-m-queue-list';
+     protected static SubNavigationPosition $subNavigationPosition=SubNavigationPosition::End;
 
     public static function form(Form $form): Form
     {
@@ -37,64 +44,63 @@ class ProductResource extends Resource
 
             ->schema([
                 Forms\Components\Grid::make()
-                ->schema([
-                    TextInput::make('title')
-                    ->live(onBlur: true)->required()
-                    ->afterStateUpdated(function (string $operation, $state, callable $set) {
-                        $set('slug', str::slug($state));
-                    }),
-                TextInput::make('slug')->required(),
-                Select::make('department_id')
-                    ->relationship('department', 'name')
-                    ->label(__('Department'))
-                    ->searchable()
-                    ->reactive()
-                    ->preload()
-                    ->afterStateUpdated(function(callable $set){
-                        $set('category_id',null);
-                    }),
-                Select::make('category_id')
-                ->relationship(
-                   'category',
-                   'name',
-                   function(Builder $query ,callable $get){
-                    $departmentId=$get('department_id');
-                    if($departmentId){
-                        $query->where('department_id',$departmentId);
-                    }
-                   }
-                )
-                ->label(__('Category'))
-                ->preload()
-                ->searchable()
-                ->required()
-                ,
+                    ->schema([
+                        TextInput::make('title')
+                            ->live(onBlur: true)->required()
+                            ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                                $set('slug', str::slug($state));
+                            }),
+                        TextInput::make('slug')->required(),
+                        Select::make('department_id')
+                            ->relationship('department', 'name')
+                            ->label(__('Department'))
+                            ->searchable()
+                            ->reactive()
+                            ->preload()
+                            ->afterStateUpdated(function (callable $set) {
+                                $set('category_id', null);
+                            }),
+                        Select::make('category_id')
+                            ->relationship(
+                                'category',
+                                'name',
+                                function (Builder $query, callable $get) {
+                                    $departmentId = $get('department_id');
+                                    if ($departmentId) {
+                                        $query->where('department_id', $departmentId);
+                                    }
+                                }
+                            )
+                            ->label(__('Category'))
+                            ->preload()
+                            ->searchable()
+                            ->required(),
 
-                ]),
+                    ]),
                 Forms\Components\RichEditor::make('description')
-                ->required()
-                ->toolbarButtons([
-                    'blockquote',   // Blockquote button
-                    'bold',         // Bold text button
-                    'bulletList',   // Unordered (bullet) list button
-                    'h2',           // Heading level 2 button
-                    'h3',           // Heading level 3 button
-                    'italic',       // Italic text button
-                    'link',         // Hyperlink button
-                    'orderedList',  // Ordered (numbered) list button
-                    'redo',         // Redo button
-                    'strike',       // Strikethrough text button
-                    'underline',    // Underline text button
-                    'undo',         // Undo button
-                    'table',        // Insert table button
-                ])
-                ->columnSpan(2),
+                    ->required()
+                    ->toolbarButtons([
+                        'blockquote',   // Blockquote button
+                        'bold',         // Bold text button
+                        'bulletList',   // Unordered (bullet) list button
+                        'h2',           // Heading level 2 button
+                        'h3',           // Heading level 3 button
+                        'italic',       // Italic text button
+                        'link',         // Hyperlink button
+                        'orderedList',  // Ordered (numbered) list button
+                        'redo',         // Redo button
+                        'strike',       // Strikethrough text button
+                        'underline',    // Underline text button
+                        'undo',         // Undo button
+                        'table',        // Insert table button
+                    ])
+                    ->columnSpan(2),
                 TextInput::make('price')->required()->nullable(),
                 TextInput::make('quantity')->integer(),
                 Select::make('status')
-                ->options(ProductStatusEnum::labels())
-                ->default(ProductStatusEnum::Draft->value)
-                ->required(),
+                    ->options(ProductStatusEnum::labels())
+                    ->default(ProductStatusEnum::Draft->value)
+                    ->required(),
 
 
 
@@ -105,9 +111,14 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
+                SpatieMediaLibraryImageColumn::make('images')
+                               ->collection('images')
+                               ->limit(1)
+
+                               ->conversion('thumb'),
 
                 TextColumn::make('title')->searchable()->words(10)->sortable(),
-                TextColumn::make('status')->badge()->color(fn ($state) => ProductStatusEnum::colors()[$state] ?? 'default'),
+                TextColumn::make('status')->badge()->color(fn($state) => ProductStatusEnum::colors()[$state] ?? 'default'),
 
                 TextColumn::make('department.name'),
                 TextColumn::make('category.name'),
@@ -116,7 +127,7 @@ class ProductResource extends Resource
             ])
             ->filters([
                 SelectFilter::make('status')->options(ProductStatusEnum::labels()),
-                SelectFilter::make('department_id')->relationship('department','name'),
+                SelectFilter::make('department_id')->relationship('department', 'name'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -142,11 +153,21 @@ class ProductResource extends Resource
             'index' => Pages\ListProducts::route('/'),
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
+            'images'=>Pages\ProductsImages::route('/{record}/images')
         ];
     }
-    // public static function canViewAny(): bool
-    // {
-    //     $user=Filament::auth()->user();
-    //     return $user&& $user->hasAnyRole([RolesEnum::Admin,RolesEnum::Vendor]);
-    // }
+
+    public static function getRecordSubNavigation(Page $page): array
+    {
+        return
+            $page->generateNavigationItems(
+                [
+                    EditProduct::class,
+                    ProductsImages::class
+
+                ]
+
+            )
+        ;
+    }
 }
